943cf7f17f204759290a2c9225166a9c7ed441d2
kairitsims <kairitsims@gmail.com>
2017-09-23 22:40:43 +0300

    Testid

--- WeatherForecast/src/main/java/ee/ttu/IAY0361/main/Weather.java
+++ WeatherForecast/src/main/java/ee/ttu/IAY0361/main/Weather.java
@@ -1,5 +1,39 @@
 package ee.ttu.IAY0361.main;
 
+import java.util.Date;
+
 public class Weather {
+	
+	private Date todaysDate;
+	private Date todayPlusOneDate;
+	private Date todayPlusTwoDate;
+	private Date todayPlusThreeDate;
+	private String coordinates;
+	private double currentTemperature;
+	
+	
+	
+	public double getCurrentTemperature() {
+		return currentTemperature;
+	}
+	public Date getTodaysDate() {
+		return todaysDate;
+	}
+	public Date getTodayPlusOneDate() {
+		return todayPlusOneDate;
+	}
+	public Date getTodayPlusTwoDate() {
+		return todayPlusTwoDate;
+	}
+	public Date getTodayPlusThreeDate() {
+		return todayPlusThreeDate;
+	}
+	public String getCoordinates() {
+		return coordinates;
+	}
+	
+	
+
+	
 
 }
--- WeatherForecast/src/test/java/ee/ttu/IAY0361/test/WeatherTest.java
+++ WeatherForecast/src/test/java/ee/ttu/IAY0361/test/WeatherTest.java
@@ -2,18 +2,38 @@ package ee.ttu.IAY0361.test;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
+
 import org.junit.Test;
 
-public class WeatherTest {
+import ee.ttu.IAY0361.main.Weather;
 
+public class WeatherTest {
+	
+	Weather weather = new Weather();
+	
 	@Test
-	public void testIfTempIsNotNull() {
-		fail("Not yet implemented");
+	public void testIfTodaysDataIsAvailable() {
+		Date date = new Date();
+		assertEquals(weather.getTodaysDate(), date.getTime());
 	}
 	
 	@Test
-	public void testIfThreeDaysForecastIsAvailable() {
-		fail("Not yet implemented");
+	public void testIfThreeNextDaysDataIsAvilable() {
+		Date date = new Date();
+		assertEquals(weather.getTodayPlusOneDate(), date.getTime() + 86400000);
+		assertEquals(weather.getTodayPlusTwoDate(), date.getTime() + 2*86400000);
+		assertEquals(weather.getTodayPlusThreeDate(), date.getTime() + 3*86400000);
+	}
+	
+	@Test
+	public void testCoordinatesAreAvailable() {
+		//koordinaadid on kujul lat: 59.437, lon: 24.7535
+		assertNotNull(weather.getCoordinates());
+		assertTrue(weather.getCoordinates().contains("lat:"));
+		assertTrue(weather.getCoordinates().contains("lon:"));
+		assertTrue(weather.getCoordinates().length() <= 26);
+		
 	}
 	
 	@Test
